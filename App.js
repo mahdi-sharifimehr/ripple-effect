50d5d07460a63a51308bc025719798b2d74ad75e
Mahdi <mathfractal@yahoo.com>
2021-01-28 14:06:17 +0330

    commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.body}>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
