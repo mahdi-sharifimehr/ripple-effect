3009bb81663051096efcc537f239403f3c95c713
sharifimehr.mahdi <sharifimehr.mahdi@gmail.com>
2021-02-03 12:21:05 +0330

    commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {
+  Pressable,
   StyleSheet,
   View,
+  Text,
 } from 'react-native';
+import Ripple from 'react-native-material-ripple';
 
 const App = () => {
   return (
     <View style={styles.body}>
+      <Ripple
+        style={styles.button}
+        rippleColor='#f0f'
+        rippleOpacity={0.9}
+        rippleDuration={2000}
+        rippleSize={500}
+        rippleCentered={true}
+      >
+        <Text style={styles.text}>Press me</Text>
+      </Ripple>
     </View>
   );
 };
@@ -18,6 +31,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 250,
+    height: 100,
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30
+  }
 });
 
 export default App;
